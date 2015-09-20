075cf658605bc31f6fdb0babe2691904d095a1c1
FEvelychko <maxdevelopenko@gmail.com>
2015-09-20 20:08:53 +0300

    getting keybord values

--- scripts/app.js
+++ scripts/app.js
@@ -2,3 +2,28 @@
  * Created by Maksym on 20.09.2015.
  */
 
+    function Calculator(){
+        this.span = document.getElementById('keys').children;
+        return this;
+
+    };
+
+    Calculator.prototype.isNumber = function(isNumber){
+        return !isNaN(isNumber);
+    };
+
+    Calculator.prototype.setEvent = function() {
+        for (var x = 0; x < this.span.length; x++) {
+            if (this.isNumber(this.span[x].innerHTML)) {
+                this.span[x].addEventListener('click', this.hello);
+            }
+        }
+        return this;
+    };
+
+    Calculator.prototype.hello = function(){
+        console.log('sdsd');
+    };
+
+    var calculator = new Calculator();
+    calculator.setEvent();
